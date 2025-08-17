ededa2f9cbd7ffd352b1fdf90746a1d865bd4b76
금예빈 <147026802+bebeaubn@users.noreply.github.com>
2025-08-18 02:03:08 +0900

    2025.08.18

--- day01/src/exam01/Ex01.java
+++ day01/src/exam01/Ex01.java
@@ -1,11 +1,13 @@
-package exam01;
+package day01.src.exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
         int num; // 변수를 선언한다
         num = 10; // 변수선언후 값을 대입하는것을 초기화라고 한다
 
-        int num2 = 10;
+        int num2 = 10;  // 선언과 동시에 초기화를 한다 -> 주로 이렇게 쓰인다
+
+        double num3 = 10.1234;
 
     }
 }
--- day01/src/exam01/Ex02.java
+++ day01/src/exam01/Ex02.java
@@ -1,8 +1,9 @@
-package exam01;
+package day01.src.exam01;
 
 public class Ex02 {
     public static void main(String[] args) {
-        char ch = 'A';
-        
+        char ch1 = 'A'; // 문자형 자료형
+        System.out.println(ch1); // A
+
     }
 }
